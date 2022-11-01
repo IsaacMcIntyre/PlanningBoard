715a060d35ca8efb330bbad1023e0b9bfac7cb67
Isaac McIntyre <isaacmmcintyre@gmail.com>
2022-11-01 22:16:45 +0000

    Added basic page that loops through hardcoded data.

---
+++ planning-board-fe/components/TodoList.vue
@@ -0,0 +1,13 @@
+<script setup>
+const todoItems = [
+  {name: "Service boiler"}, {name: "Gas check"}, {name: "Wash dishes"}
+]
+
+</script>
+
+<template>
+  <h2><slot /></h2>
+  <ul v-if="todoItems">
+    <li v-for="item in todoItems">{{item.name}}<button>Delete</button></li>
+  </ul>
+</template>
---
+++ planning-board-fe/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Home Dashboard</h1>
+    <TodoList>Todo Items</TodoList>
+  </div>
+</template>
